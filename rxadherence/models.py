368c925018d1f51831288f30d53da4fbaa44df8d
Stephen Kelly <stevekm@users.noreply.github.com>
2019-04-26 12:36:07 -0400

    make drug names unique

--- rxadherence/models.py
+++ rxadherence/models.py
@@ -5,7 +5,7 @@ class Drug(models.Model):
     """
     A test model to make sure the database workse
     """
-    name = models.CharField(max_length=255)
+    name = models.CharField(unique = True, max_length=255)
     imported = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     def __str__(self):
